4c9e17634b4f2036e9672498f9879ca272533c14
Vinicius Sales de Souza <viniciussds4@gmail.com>
2025-07-07 20:35:00 -0300

    Projeto Conta Bancaria

---
+++ menu.ts
@@ -0,0 +1,109 @@
+//import { console } from "inspector";
+import readlinesync = require("readline-sync");
+import { colors } from "./src/util/colors";
+
+export function main(){
+
+    let opcao: number;
+
+    while (true){
+
+        console.log(colors.bg.black, colors.fg.yellow,
+                    "**********************************************");
+        console.log("                                              ");
+        console.log("          BANCO DO BRAZIL COM Z               ");
+        console.log("                                              ");
+        console.log("**********************************************");
+        console.log("                                              ");
+        console.log("       1 - Criar Conta                        ");
+        console.log("       2 - Listar todas as Contas             ");
+        console.log("       3 - Buscar Conta por Numero            ");
+        console.log("       4 - Atualizar Dados da Conta           ");
+        console.log("       5 - Apagar Conta                       ");
+        console.log("       6 - Sacar                              ");
+        console.log("       7 - Depositar                          ");
+        console.log("       8 - Transferir valores entre contas    ");
+        console.log("       9 - Sair                               ");
+        console.log("                                              ");
+        console.log("**********************************************");
+        console.log("                                              ", 
+            colors.reset);
+
+        console.log("Entre com uma opção desejada: ");
+        opcao = readlinesync.questionInt("");
+
+        if (opcao == 9) {
+            console.log(colors.fg.greenstrong,
+                "\nBanco do Brazil com Z - O seu  Futuro começa aqui!");
+            sobre(); 
+            console.log(colors.reset, "");
+            process.exit(0);
+        }
+
+        switch (opcao) {
+            case 1:
+                console.log(colors.fg.whitestrong,
+                    "\n\nCriar Conta\n\n", colors.reset);
+                keyPress()
+                break;
+            case 2:
+                console.log(colors.fg.whitestrong,
+                    "\n\nListar todas as Contas\n\n", colors.reset);
+                keyPress()
+                break;
+            case 3:
+                console.log(colors.fg.whitestrong,
+                    "\n\nBuscar Conta por Numero\n\n", colors.reset);
+                keyPress()
+                break;
+            case 4:
+                console.log(colors.fg.whitestrong,
+                    "\n\nAtualizar Dados da Conta\n\n", colors.reset);
+                keyPress()
+                break;
+            case 5:
+                console.log(colors.fg.whitestrong,
+                    "\n\nApagar Conta\n\n", colors.reset);
+                keyPress()
+                break;
+            case 6:
+                console.log(colors.fg.whitestrong,
+                    "\n\nSacar\n\n", colors.reset);
+                keyPress()
+                break;
+            case 7:
+                console.log(colors.fg.whitestrong,
+                    "\n\nDepositar\n\n", colors.reset);
+                keyPress()
+                break;
+            case 8:
+                console.log(colors.fg.whitestrong,
+                    "\n\nTransferir valores entre contas\n\n", colors.reset);
+                keyPress()
+                break;
+            default:
+                console.log(colors.fg.whitestrong,
+                    "\nOpção Inválida!\n", colors.reset);
+                keyPress()
+                break;
+        }
+    }
+}
+
+
+export function sobre(): void {
+    console.log("\n**********************************************");
+    console.log("Projeto Desenvolvido por: ");
+    console.log("Generation Brasil - generation@generation.org");
+    console.log("github.com/conteudoGeneration");
+    console.log("**********************************************");
+}
+
+    function keyPress(): void {
+        console.log(colors.reset, "");
+        console.log("\nPressione enter para continuar...");
+        readlinesync.prompt();
+    }
+    
+ main();
+
