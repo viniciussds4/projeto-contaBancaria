12d75f78c1afb465be5c8fa707732e86a91e1a14
Vinicius Sales de Souza <viniciussds4@gmail.com>
2025-07-13 15:50:02 -0300

    Nova classe Model/Conta

--- menu.ts
+++ menu.ts
@@ -1,10 +1,18 @@
 //import { console } from "inspector";
 import readlinesync = require("readline-sync");
 import { colors } from "./src/util/colors";
+import { Conta } from "./src/model/Conta";
 
 export function main(){
 
     let opcao: number;
+
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta. visualizar();
+    conta.sacar(35000);
+    conta.visualizar();
+    conta.depositar(10000);
+    conta.visualizar();
 
     while (true){
 
